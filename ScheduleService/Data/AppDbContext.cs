224d4b9896cc7c868a4c6a929e17814843d8ad91
Vincen <vincenhayate123@gmail.com>
2024-11-21 19:36:26 +0700

    Add db context

--- ScheduleService/Data/AppDbContext.cs
+++ ScheduleService/Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace ScheduleService.Data
+﻿using Microsoft.EntityFrameworkCore;
+using ScheduleService.Models;
+
+namespace ScheduleService.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Schedule> Schedules { get; set; }
     }
 }
